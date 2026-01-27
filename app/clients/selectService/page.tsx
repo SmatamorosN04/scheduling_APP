46d70cb13798d4c0e8053d119a15bec68854aefa
Sergio Matamoros <124097535+SmatamorosN04@users.noreply.github.com>
2026-01-27 14:58:35 -0600

    start develop of selection of service

--- app/clients/selectService/page.tsx
+++ app/clients/selectService/page.tsx
@@ -1,7 +1,7 @@
 import Header from "@/app/components/header/header";
 import Footer from "@/app/components/footer/footer";
 
-export default function Booking(){
+export default function SelectService(){
     return(
         <div className='bg-[#F2EFDF] h-screen w-full'>
             <Header/>
--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,16 @@ import Footer from "@/app/components/footer/footer";
 
 export default async function Home() {
     return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-[#F2EFDF] font-sans gap-4 ">
         <Header/>
         <Link
         href="/admin/dashboard"
        className='p-2 bg-white rounded-lg hover:cursor-pointer text-black' > Go to DashBoard</Link>
+        <Link
+            href="/clients/selectService"
+            className='p-2 bg-white rounded-lg hover:cursor-pointer text-black' > Go to select Service</Link>
         <Footer/>
+
     </div>
   );
 }
